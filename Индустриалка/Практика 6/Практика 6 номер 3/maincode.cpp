8cf2dcd8f643a1e4d9eaa4bd56aa06e0ef339b44
Maria040821 <126405446+Maria040821@users.noreply.github.com>
2023-03-31 23:25:19 +0300

    Add files via upload

---
+++ Индустриалка/Практика 6/Практика 6 номер 3/maincode.cpp
@@ -0,0 +1,31 @@
+﻿#include <array>
+
+typedef std::array<char, 9> tictactoe_status;
+
+tictactoe_status const strategy_x[] = 
+{
+   {1,0,0,0,0,0,0,0,0},
+   {1,2,0,0,1,0,0,0,0},
+   {1,2,2,0,1,0,0,0,1},
+   {1,2,0,2,1,0,0,0,1},
+   // ...
+};
+
+tictactoe_status const strategy_o[] = 
+{
+   {2,0,0,0,1,0,0,0,0},
+   {2,2,1,0,1,0,0,0,0},
+   {2,2,1,2,1,0,1,0,0},
+   {2,2,1,0,1,2,1,0,0},
+   // ...
+};
+
+tictactoe_status const strategy_x[] = 
+{
+#include "strategy_x.h"
+};
+
+tictactoe_status const strategy_o[] = 
+{
+#include "strategy_o.h"
+};
